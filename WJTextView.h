cbf323b4e4fb1fbdbe8c12e8e869f6c6819fa53c
Always Delighted <wwj405098620@126.com>
2015-06-24 09:27:45 +0800

    1.0
    First Code

---
+++ WJTextView.h
@@ -0,0 +1,13 @@
+//
+//  WJTextView.h
+//  WJTextView
+//
+//  Created by fosung_newMac on 15/6/23.
+//  Copyright (c) 2015年 fosung_newMac. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface WJTextView : UITextView
+@property (nonatomic, strong) NSString *placeHolder;
+@end
---
+++ WJTextView.m
@@ -0,0 +1,50 @@
+//
+//  WJTextView.m
+//  WJTextView
+//
+//  Created by fosung_newMac on 15/6/23.
+//  Copyright (c) 2015年 fosung_newMac. All rights reserved.
+//
+
+#import "WJTextView.h"
+
+@implementation WJTextView
+
+{
+    BOOL _editing;
+    BOOL _hasText;
+}
+
+
+-(void)setPlaceHolder:(NSString *)placeHolder
+{
+    _placeHolder = placeHolder;
+    [[NSNotificationCenter defaultCenter]addObserver:self selector:@selector(editBegin) name:UITextViewTextDidBeginEditingNotification object:nil];
+    [[NSNotificationCenter defaultCenter] addObserver:self selector:@selector(editEnd) name:UITextViewTextDidEndEditingNotification object:nil];
+}
+
+-(void)setText:(NSString *)text
+{
+    [super setText:text];
+    _hasText = self.text&&self.text.length>0;
+    [self setNeedsDisplay];
+}
+- (void)drawRect:(CGRect)rect {
+    [super drawRect:rect];
+    if ((!_editing)&&self.placeHolder&&self.placeHolder.length>0&&!_hasText) {
+        [_placeHolder drawAtPoint:CGPointMake(5, 5) withAttributes:@{NSFontAttributeName:[UIFont systemFontOfSize:10],NSForegroundColorAttributeName:[UIColor lightGrayColor]}];
+    }
+}
+
+- (void)editBegin
+{
+    _editing = YES;
+    [self setNeedsDisplay];
+}
+- (void)editEnd
+{
+    _editing = NO;
+    _hasText = self.text&&self.text.length>0;
+    [self setNeedsDisplay];
+}
+@end
